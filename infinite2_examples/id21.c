665262841d1c2675c064cd3ba0200ed9feec8812
Oren Sultan <orensultan@meta.com>
2026-03-10 11:29:50 +0200

    Update id21.c

--- infinite2_examples/id21.c
+++ infinite2_examples/id21.c
@@ -1,16 +1,16 @@
 typedef struct s_ent {
-  unsigned int val;
-  struct s_ent *next;
-}	       ent_t;
+    unsigned int val;
+    struct s_ent *next;
+} ent_t;
 
-id19(*ent_t);
+void id19(ent_t *param);
 
 void id21() {
-  ent_t one;
-  ent_t two;
-  one.val = 1;
-  one.next = &two;
-  two.val = 2;
-  two.next = &one;
-  id19(&one);
+    ent_t one;
+    ent_t two;
+    one.val = 1;
+    one.next = &two;
+    two.val = 2;
+    two.next = &one;
+    id19(&one);
 }
